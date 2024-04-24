c2bad9dbf9c51992a5351b4d2a0f25557de18075
Thomas Wiese <mail@thomaswiese.de>
2024-04-24 17:06:58 +0200

    add script synogit.sh to create git repo on synology server

---
+++ synogit.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+COMMAND_LINE_OPTIONS_HELP="
+Command line options:\n
+  -s server       ssh host\n
+  -p port         ssh port, default 22\n
+  -u user         ssh user, default admin\n
+  -r repository   name of git repository to create\n
+  -d path         directory where to create the project, default /volume1/git\n
+\n
+e.g. `basename $0` -s 192.168.1.467 -r myrepo\n
+"
+
+error () {
+    echo -e $COMMAND_LINE_OPTIONS_HELP
+    exit -1
+}
+
+while getopts s:r:u:p:d flag
+do
+	case "${flag}" in
+		s) optserver=${OPTARG};;
+		p) optport=${OPTARG};;
+		u) optuser=${OPTARG};;
+		r) optrepo=${OPTARG};;
+		d) optgitdir=${OPTARG};;
+		*) error;;
+	esac
+done
+
+if [ -z "${optserver}" ]; then
+	echo "server option is missing"; error
+fi
+if [ -z "${optrepo}" ]; then
+	echo "repository is missing"; error
+fi
+
+server=${optuser:=admin}@$optserver
+port=${optport:=22}
+repo=${optrepo}
+gitdir=${optgitdir:=/volume1/git}
+
+echo "Connecting to $server on port $port and create repository $repo..."
+
+ssh $server -p $port "
+  cd $gitdir
+  mkdir $repo
+  sudo chown git $repo
+  cd $repo
+  git init --bare
+  sudo chown -R git *
+"
+echo "created repo $repo."
